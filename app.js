ee1eb4660e671a826c0b02fb12294364b15a0175
developerMohib <92154638+developerMohib@users.noreply.github.com>
2023-05-28 16:42:46 +0600

    Just Function Demo installation

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// Create a function
+// Build a calculator using function
+
+// resulttraction Function Creation
+
+
+function addition(num1, num2) {
+  
+  let result = num1 + num2;
+  document.write(`addition = `+ result + "<br>")
+
+}
+function minasKoron(num1, num2) {
+  
+  let result = num1 + num2;
+  document.write(`minasKoron = `+ result + "<br>")
+
+}
+function multiply(num1, num2) {
+  
+  let result = num1 + num2;
+  document.write(`multiply = `+ result + "<br>")
+
+}
+function vagKoron(num1, num2) {
+  
+  let result = num1 + num2;
+  document.write(`vagKoron = `+ result + "<br>")
+
+}
+addition(4,6);
+minasKoron(8, 2);
+multiply(2,2);
+vagKoron(4,2);
+
